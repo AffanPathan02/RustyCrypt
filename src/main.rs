a711e8306f04abc60dc7ff3c47a5ebf63ee093ef
Affan Pathan <pathan.affan02@gmail.com>
2024-01-27 14:40:54 +0530

    added simple substitution cipher

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,23 @@ fn main() {
         Ok(content)=>{
             println!("Content of the file:");
             println!("{}",content);
+            let operation= get_user_input("Enter encrypt or decrypt :");
+            match operation.to_lowercase().as_str(){
+                "encrypt"=>{
+                    let encrypted_contect=encrypt(&content);
+                    println!("Encrypted content:");
+                    println!("{}",encrypted_contect)
+                },
+                "decrypt"=>{
+                    let decrypted_content=decrypt(&content);
+                    println!("decrypted content:");
+                    println!("{}",decrypted_content)
+                },
+                _=>{
+                    eprintln!("Invalid operation. Use 'encrypt', 'decrypt', or 'none'.");
+                    return;
+                }
+            }
         }
         Err(err)=>{
             eprintln!("Error reading the file:{}",err);
@@ -29,4 +46,34 @@ fn read_file(file_path:&str)->io::Result<String>{
     let mut content=String::new();
     file.read_to_string(&mut content)?;
     Ok(content)
+}
+
+fn encrypt(content: &str)->String{
+    let encrypted_content:String=content
+        .chars()
+        .map(|c|{
+            if c.is_ascii_alphabetic() {
+                let base=if c.is_ascii_lowercase(){b'a'}else { b'A' };
+                (((c as u8 - base + 3) % 26) + base) as char
+            }else{
+                c
+            }
+        })
+        .collect();
+    encrypted_content
+}
+
+fn decrypt(content: &str) -> String {
+    let decrypted_content: String = content
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphabetic() {
+                let base = if c.is_ascii_lowercase() { b'a' } else { b'A' };
+                (((c as u8 - base + 23) % 26) + base) as char
+            } else {
+                c
+            }
+        })
+        .collect();
+    decrypted_content
 }
